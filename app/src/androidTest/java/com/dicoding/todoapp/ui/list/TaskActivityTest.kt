17c955d89e1ea446b6884c003d97bede7f597735
alwanfauzy <alwanfauzi13@gmail.com>
2022-09-06 00:06:27 +0700

    Solve TODO 16

--- app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
+++ app/src/androidTest/java/com/dicoding/todoapp/ui/list/TaskActivityTest.kt
@@ -1,6 +1,41 @@
 package com.dicoding.todoapp.ui.list
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.dicoding.todoapp.R
+import com.dicoding.todoapp.ui.add.AddTaskActivity
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
 //TODO 16 : Write UI test to validate when user tap Add Task (+), the AddTaskActivity displayed
 class TaskActivityTest {
 
+    @get:Rule
+    var activityRule = ActivityScenarioRule(TaskActivity::class.java)
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+        Intents.init()
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun tearDown() {
+        Intents.release()
+    }
+
+    @Test
+    fun whenUserClickAddTask_ShouldDisplayAddTaskActivity() {
+        onView(withId(R.id.fab)).perform(click())
+
+        intended(hasComponent(AddTaskActivity::class.java.name))
+    }
 }
