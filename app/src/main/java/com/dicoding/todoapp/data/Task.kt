7e2b4ed1973647da08ffa70f282b935ac7745e2b
alwanfauzy <alwanfauzi13@gmail.com>
2022-09-05 21:20:25 +0700

    Solve TODO 1

--- app/src/main/java/com/dicoding/todoapp/data/Task.kt
+++ app/src/main/java/com/dicoding/todoapp/data/Task.kt
@@ -1,10 +1,18 @@
 package com.dicoding.todoapp.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
 //TODO 1 : Define a local database table using the schema in app/schema/tasks.json
+@Entity(tableName = "tasks")
 data class Task(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val title: String,
     val description: String,
     val dueDateMillis: Long,
+
+    @ColumnInfo(name = "completed")
     val isCompleted: Boolean = false
 )
